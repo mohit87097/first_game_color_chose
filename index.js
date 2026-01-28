dacd0632a589f10ea49735140277697b86464fc9
mohit kumar <mohitkumar80845565@gmail.com>
2026-01-28 12:19:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const colourNames = [
   "Pink","Brown","Lime","Olive","Teal","Navy","Maroon","Silver"
 ];
 
-let winingScore = 3;
+let winingScore = 5;
 let targetcolor = "";
 let score = 0;
 let timer_in = 120;
